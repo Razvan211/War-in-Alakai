5200d4d46e1f40ca1cb2f68a3599fcdd575353d7
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-02-02 16:01:08 +0200

    Units get added to the list when pressed on them with the mouse cursor
    If we hold the left shift and press on other units, the units will be added to the list.
    Pressing on an empty space ("Ground") the list gets empty.

--- Assets/Scripts/Units/SelectUnit.cs
+++ Assets/Scripts/Units/SelectUnit.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SelectUnit : MonoBehaviour
 {
+    //list to store all units
     public List<GameObject> units = new List<GameObject>();
+
+    //list to store selected units
     public List<GameObject> selectedUnits = new List<GameObject>();
 
 
@@ -28,7 +31,8 @@ public class SelectUnit : MonoBehaviour
 
     public void ClickSelection (GameObject coveredUnits)
     {
-
+        DeselectAll();
+        selectedUnits.Add(coveredUnits);
     }
 
     public void DragSelect(GameObject coveredUnits)
@@ -38,12 +42,18 @@ public class SelectUnit : MonoBehaviour
     
     public void ShiftClickSelect(GameObject coveredUnits)
     {
-
+        if (!selectedUnits.Contains(coveredUnits))
+        {
+            selectedUnits.Add(coveredUnits);
+        }else
+        {
+            selectedUnits.Remove(coveredUnits);
+        }
     }
 
     public void DeselectAll()
     {
-
+        selectedUnits.Clear();
     }
 
     public void Deselect(GameObject unitsToUncover)
