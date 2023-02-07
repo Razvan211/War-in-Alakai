52d51578d2d4cb895be5f498a1363e8b9317606e
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-02-07 19:57:04 +0200

    Incorporated Buildings work in this branch
    Added Scripts for Storing building data, manage buildings

---
+++ Assets/Scripts/Buildings/Globals.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals : MonoBehaviour
+{
+    public static BuildingsData[] Building_Data = new BuildingsData[]
+    {
+        new BuildingsData("LumberMill", 100)
+
+    };
+}
