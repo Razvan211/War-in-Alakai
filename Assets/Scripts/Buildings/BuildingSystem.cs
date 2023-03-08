2495f9d80138426885bd485e97fa3fba53589e99
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-03-08 12:12:50 +0200

    Update BuildingSystem.cs

--- Assets/Scripts/Buildings/BuildingSystem.cs
+++ Assets/Scripts/Buildings/BuildingSystem.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BuildingSystem : MonoBehaviour
 {
+    private PlaceableBuilding buildingToPlace;
+
+    public GameObject lumberMill;
+    public GameObject mageTower;
+    public GameObject workshop;
     // Start is called before the first frame update
     void Start()
     {
