c9beb87b36e216dfc171c7ed8b26068715c29cf7
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-03-15 16:19:51 +0200

    Buildings can be placed through UI

---
+++ Assets/Scripts/UI elements/UIBuilding/BuildLumberMill.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildLumberMill : MonoBehaviour
+{
+    public Vector3 place;
+    public GameObject prefab;
+
+    private RaycastHit hit;
+
+    public bool placeNow;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && placeNow == true)
+        {
+
+
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                if (hit.transform.tag == "Terrain")
+                {
+                    place = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                    Instantiate(prefab, place, Quaternion.identity);
+
+                    placeNow = false;
+                }
+            }
+        }
+
+    }
+
+    public void PlaceCube()
+    {
+
+        placeNow = true;
+    }
+}
