f047261a72b01b9b6f0656afcd4232c1e19bc601
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-04-27 23:05:26 +0300

    Update MoveToEnemyAgent.cs

--- Assets/Scripts/AI Agent/MoveToEnemyAgent.cs
+++ Assets/Scripts/AI Agent/MoveToEnemyAgent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
@@ -8,14 +9,32 @@ using Unity.MLAgents.Sensors;
 public class MoveToEnemyAgent : Agent
 {
     [SerializeField] private Transform targetTr;
+    [SerializeField] private NavMeshAgent agent;
+    RaycastHit hit;
+
+    public override void OnEpisodeBegin()
+    {
+        agent.transform.position = new Vector3(0, 0, 7.4f);
+    }
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.position);
+        sensor.AddObservation(agent.transform.position);
         sensor.AddObservation(targetTr.transform.position);
 
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Debug.Log(actions.ContinuousActions[0]);
+        
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        agent.SetDestination(targetTr.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SetReward(1f);
+
+        EndEpisode();
     }
 }
