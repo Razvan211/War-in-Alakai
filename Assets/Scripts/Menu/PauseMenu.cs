75325113935a853eb6b3ee07563f3783ca64601c
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2023-05-09 13:15:23 +0300

    Main Menu, Pause Menu Added
    There is a bug with the menu, when going back from the game in the menu and back the input manager gets emptied and it acts as the LMB is pressed all the time. The bug does not appear when the player enters the game for the first time.

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RN.WIA.Menu
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public static bool gameIsPaused = false;
+
+        private string sceneMenu = "Menu";
+
+        public GameObject pauseMenuUI;
+        public GameObject gameUI;
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (gameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            gameUI.SetActive(true);
+            Time.timeScale = 1f;
+            gameIsPaused = false;
+        }
+
+        void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            gameUI.SetActive(false);
+            Time.timeScale = 0f;
+            gameIsPaused = true;
+        }
+
+        public void LoadMenu()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(sceneMenu);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+
+}
